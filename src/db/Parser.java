a29edc279daceb2ccaadb800c6e2bdbafdd5b5eb
Adrián Mirón Cao <magicadri@gmail.com>
2016-10-09 19:09:03 +0200

    TestPush merge

--- src/db/Parser.java
+++ src/db/Parser.java
@@ -1,5 +1,5 @@
 package db;
 
 public class Parser {
-	//Operaciones de la base de datos y pasar a listas/arrays los datos para utilizar en la lógica y en la interfaz
+	//Operaciones de la base de datos y pasar a listas/arrays los datos para utilizar en la l?gica y en la interfaz
 }
