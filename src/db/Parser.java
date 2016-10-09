4d6bdb89c676c2b2cf6371a5c6ee59813952d3ba
Adrián Mirón Cao <magicadri@gmail.com>
2016-10-09 19:16:04 +0200

    Trying to fix repository scheme

--- src/db/Parser.java
+++
@@ -1,5 +0,0 @@
-package db;
-
-public class Parser {
-	//Operaciones de la base de datos y pasar a listas/arrays los datos para utilizar en la l?gica y en la interfaz
-}
--- src/main/java/db/Parser.java
+++ src/main/java/db/Parser.java
@@ -1,5 +1,4 @@
 package db;
 
 public class Parser {
-	//Operaciones de la base de datos y pasar a listas/arrays los datos para utilizar en la lógica y en la interfaz
 }
