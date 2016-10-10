b6c8777600187132e59a71006bce5f60fa461b4a
Joni <Jonivalles@gmail.com>
2016-10-10 12:29:33 +0200

    DateChooser funcionando para coger la fecha. Falta hacer el split para quitar la hora.

--- src/main/java/gui/VentanaCalendar.java
+++ src/main/java/gui/VentanaCalendar.java
@@ -7,10 +7,16 @@ import javax.swing.JButton;
 import javax.swing.JDialog;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import com.toedter.calendar.JDateChooser;
+import java.beans.PropertyChangeListener;
+import java.beans.PropertyChangeEvent;
+import javax.swing.JTextField;
 
 public class VentanaCalendar extends JDialog {
 
 	private final JPanel contentPanel = new JPanel();
+	private JDateChooser dateChooser;
+	private JTextField tf;
 
 	/**
 	 * Launch the application.
@@ -31,9 +37,30 @@ public class VentanaCalendar extends JDialog {
 	public VentanaCalendar() {
 		setBounds(100, 100, 450, 300);
 		getContentPane().setLayout(new BorderLayout());
-		contentPanel.setLayout(new FlowLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
+		contentPanel.setLayout(null);
+		contentPanel.add(getDateChooser());
+		contentPanel.add(getTf());
 	}
-
+	private JDateChooser getDateChooser() {
+		if (dateChooser == null) {
+			dateChooser = new JDateChooser();
+			dateChooser.addPropertyChangeListener(new PropertyChangeListener() {
+				public void propertyChange(PropertyChangeEvent arg0) {
+					tf.setText(String.valueOf(dateChooser.getDate()));
+				}
+			});
+			dateChooser.setBounds(142, 64, 95, 20);
+		}
+		return dateChooser;
+	}
+	private JTextField getTf() {
+		if (tf == null) {
+			tf = new JTextField();
+			tf.setBounds(76, 136, 86, 20);
+			tf.setColumns(10);
+		}
+		return tf;
+	}
 }
